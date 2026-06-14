ab0a29124a93014944a65ac7ba8a67f65c11fd61
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-14 17:22:02 +0530

    setting up axios in signup page

--- frontend/src/pages/Sign.jsx
+++ frontend/src/pages/Sign.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-const Signup = () => {
-  return (<>Signup page</>);
+const Sign = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/api/signup", {
+        name,
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+      console.log(error.response?.data);
+    }
+  };
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={handleSignup}>Signup</button>
+    </>
+  );
 };
 
-export default Signup;
+export default Sign;
