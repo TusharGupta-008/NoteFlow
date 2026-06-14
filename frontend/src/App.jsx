216ffde9aafba39ef8d1fd489e0e8145a18bc0f8
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-14 05:53:26 +0530

    connecting pages to the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+import Sign from "./pages/Sign";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CreateNote from "./pages/CreateNote";
+import Signup from "./pages/Sign";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<CreateNote />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
