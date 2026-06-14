3d2fe9a2fcc94669492198178137e0d91d5920c5
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-15 02:59:30 +0530

    login page completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return (<></>);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/api/login", {
+        email,
+        password,
+      });
+      console.log(response.data);
+      localStorage.setItem("token", response.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  return (
+    <>
+      Email: <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      Password:{" "}
+      <input
+        value={password}
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </>
+  );
 };
 
 export default Login;
